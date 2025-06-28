d4c444c8d0fe0797542fdabc3af166f211db4465
xRangeroQ <82023366+xRangeroQ@users.noreply.github.com>
2025-06-29 00:16:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("0.0.0.0", 8080))
 sock.listen()
 
-cn, adr=sock.accept()
-print(adr)
-
-for i in range(10):
-  cn.send(b'SERVER:UP\n')
-
-cn.close()
-sock.close()
+while True:
+  cn, adr=sock.accept()
+  print(adr)
+  
+  for i in range(10):
+    cn.send(b'SERVER:UP\n')
+  
+  cn.close()
+  sock.close()
